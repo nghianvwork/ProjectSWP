77bce6b02d7927a0eab378f8713fc9f82a52e0b8
nghianvwork <nghianvhe180712@gmail.com>
2025-05-28 23:09:48 +0700

    dong bo add court

--- Project_SWP/src/java/Dal/DBContext.java
+++ Project_SWP/src/java/Dal/DBContext.java
@@ -9,7 +9,7 @@ public class DBContext {
     public Connection getConnection() throws ClassNotFoundException, SQLException {
         // Gắn toàn bộ thông tin kết nối vào URL
         String url = "jdbc:sqlserver://localhost:1433;"
-                   + "databaseName=qlisancaulong;"
+                   + "databaseName=SWP;"
                    + "user=sa;"
                    + "password=123;"
                    + "encrypt=true;"
